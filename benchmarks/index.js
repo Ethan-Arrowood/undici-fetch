ebc8b991057cf88f1d54923b86758a565a038342
Ethan Arrowood <ethan.arrowood@gmail.com>
2021-01-07 17:29:21 -0500

    benchmarks script

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -1,72 +1,127 @@
 const http = require('http')
-
-const { promisifyServerClose } = require('../test/testUtils')
-const { promisify } = require('util')
 const undiciFetch = require('../src/fetch')()
 const nodeFetch = require('node-fetch')
+const minipassFetch = require('minipass-fetch')
 const microtime = require('microtime')
-const fs = require('fs')
 
-function printResults (suite) {
-    for (const [testName, results] of Object.entries(suite)) {
-        console.log(`Test: ${testName}`)
-        for (const result of results) {
-            const undiciFetchTotalTime = result['undici-fetch'].timeEnd - result['undici-fetch'].timeStart
-            const nodeFetchTotalTime = result['node-fetch'].timeEnd - result['node-fetch'].timeStart
-            const percentChange = nodeFetchTotalTime / undiciFetchTotalTime * 100
-            console.log(`Iterations: ${result.iterations}\nundici-fetch total time: ${undiciFetchTotalTime}ms (${undiciFetchTotalTime/1000}s)\nnode-fetch total time: ${nodeFetchTotalTime}(${nodeFetchTotalTime/1000}s)\nPercent Change: ${percentChange}%`) 
-        }
-    }
+function printResults (results, n) {
+  console.log(`Results for ${n} subsequent requests: `)
+  const undiciFetchTT = results.undiciFetch.end - results.undiciFetch.start
+  const nodeFetchTT = results.nodeFetch.end - results.nodeFetch.start
+  const minipassFetchTT = results.minipassFetch.end - results.minipassFetch.start
+  console.log(`undici-fetch | total time: ${undiciFetchTT}µs (${(undiciFetchTT * 0.001).toFixed(3)}ms)`)
+  console.log(`node-fetch | total time: ${nodeFetchTT}µs (${(nodeFetchTT * 0.001).toFixed(3)}ms)`)
+  console.log(`minipass-fetch | total time: ${minipassFetchTT}µs (${(minipassFetchTT * 0.001).toFixed(3)}ms)`)
+  console.log(`---`)
+  const pc1 = ((undiciFetchTT - nodeFetchTT) / nodeFetchTT) * 100
+  console.log(`undici-fetch <> node-fetch percent change: ${pc1.toFixed(3)}%`)
+  const pc2 = ((undiciFetchTT - minipassFetchTT) / minipassFetchTT) * 100
+  console.log(`undici-fetch <> minipass-fetch percent change: ${pc2.toFixed(3)}%`)
 }
 
-async function benchmark () {
-    const tests = {
-        'basicGet': basicGet
-    }
+function benchmarkParallel () {
+  const N = 1000
 
-    const results = {}
-    for (const [testName, test] of Object.entries(tests)) {
-        results[testName] = []
-        for (let n = 1; n <= 100000; n *= 10) {
-            const result = await test(n)
-            // console.log(result)
-            results[testName].push(result)
-        }
-    }
-    await fs.promises.writeFile(`result-${microtime.now()}`, JSON.stringify(results))
-    printResults(results)
-}
+  const server = http.createServer((req, res) => {
+    setTimeout(() => {
+      res.end('payload')
+    }, 1)
+  })
 
-function basicGet (N) {
-    return new Promise((resolve, reject) => {
-        const server = http.createServer((req, res) => {
-            res.end()
-        })
+  const results = {
+    undiciFetch: { start: null, end: null },
+    nodeFetch: { start: null, end: null },
+    minipassFetch: { start: null, end: null }
+  }
 
-        server.listen(0, async () => {
-            const result = {
-                iterations: N,
-                'undici-fetch': {},
-                'node-fetch': {}
-            }
+  const generateRequests = (url, client) => Array.from(Array(N)).map(() => client(url).then(res => res.text()))
 
-            result['undici-fetch'].timeStart = microtime.now()
-            for (let i = 0; i < N; i++) {
-                await undiciFetch(`http://localhost:${server.address().port}`)
-            }
-            result['undici-fetch'].timeEnd = microtime.now()
-        
-            result['node-fetch'].timeStart = microtime.now()
-            for (let i = 0; i < N; i++) {
-                await nodeFetch(`http://localhost:${server.address().port}`)
-            }
-            result['node-fetch'].timeEnd = microtime.now()
-
-            await promisifyServerClose(server)()
-            await undiciFetch.close()
-            resolve(result)
-        })
+  const undiciFetchRequests = url => {
+    results.undiciFetch.start = microtime.now()
+    return Promise.all(generateRequests(url, undiciFetch)).then(() => {
+      results.undiciFetch.end = microtime.now()
+      undiciFetch.close()
     })
+  }
+  const nodeFetchRequests = url => {
+    results.nodeFetch.start = microtime.now()
+    return Promise.all(generateRequests(url, nodeFetch)).then(() => {
+      results.nodeFetch.end = microtime.now()
+    })
+  }
+  const minipassFetchRequests = url => {
+    results.minipassFetch.start = microtime.now()
+    return Promise.all(generateRequests(url, minipassFetch)).then(() => {
+      results.minipassFetch.end = microtime.now()
+    })
+  }
+
+  server.listen(0, () => {
+    const url = `http://localhost:${server.address().port}`
+
+    Promise.all([
+      undiciFetchRequests(url),
+      nodeFetchRequests(url),
+      minipassFetchRequests(url)
+    ]).then(() => {
+      server.close()
+      console.log(results)
+      printResults(results, N)
+    })
+  })
 }
 
-benchmark()
+function benchmarkSeries () {
+  const N = 10000
+
+  const server = http.createServer((req, res) => {
+    setTimeout(() => {
+      res.end('payload')
+    }, 1)
+  })
+
+  const results = {
+    undiciFetch: { start: null, end: null },
+    nodeFetch: { start: null, end: null },
+    minipassFetch: { start: null, end: null }
+  }
+
+  const undiciFetchRequests = async url => {
+    results.undiciFetch.start = microtime.now()
+    for (let i = 0; i < N; i++) {
+      await undiciFetch(url).then(res => res.text())
+    }
+    results.undiciFetch.end = microtime.now()
+    undiciFetch.close()
+  }
+  const nodeFetchRequests = async url => {
+    results.nodeFetch.start = microtime.now()
+    for (let i = 0; i < N; i++) {
+      await nodeFetch(url).then(res => res.text())
+    }
+    results.nodeFetch.end = microtime.now()
+  }
+  const minipassFetchRequests = async url => {
+    results.minipassFetch.start = microtime.now()
+    for (let i = 0; i < N; i++) {
+      await minipassFetch(url).then(res => res.text())
+    }
+    results.minipassFetch.end = microtime.now()
+  }
+
+  server.listen(0, () => {
+    const url = `http://localhost:${server.address().port}`
+
+    Promise.all([
+      undiciFetchRequests(url),
+      nodeFetchRequests(url),
+      minipassFetchRequests(url)
+    ]).then(() => {
+      server.close()
+      console.log(results)
+      printResults(results, N)
+    })
+  })
+}
+
+benchmarkSeries()
