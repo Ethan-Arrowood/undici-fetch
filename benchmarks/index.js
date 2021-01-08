d20808c8b9364ee714e017cdb4a7bf1a9699b094
Ethan Arrowood <ethan.arrowood@gmail.com>
2021-01-08 13:28:08 -0500

    make benchmark multithreaded

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -1,17 +1,16 @@
-const http = require('http')
-const undiciFetch = require('../src/fetch')()
-const nodeFetch = require('node-fetch')
-const minipassFetch = require('minipass-fetch')
-const microtime = require('microtime')
+'use strict'
+
+const { createServer } = require('http')
+const { Worker, isMainThread, parentPort, workerData } = require('worker_threads')
 
 function printResults (results, n) {
   console.log(`Results for ${n} subsequent requests: `)
-  const undiciFetchTT = results.undiciFetch.end - results.undiciFetch.start
-  const nodeFetchTT = results.nodeFetch.end - results.nodeFetch.start
-  const minipassFetchTT = results.minipassFetch.end - results.minipassFetch.start
-  console.log(`undici-fetch | total time: ${undiciFetchTT}µs (${(undiciFetchTT * 0.001).toFixed(3)}ms)`)
-  console.log(`node-fetch | total time: ${nodeFetchTT}µs (${(nodeFetchTT * 0.001).toFixed(3)}ms)`)
-  console.log(`minipass-fetch | total time: ${minipassFetchTT}µs (${(minipassFetchTT * 0.001).toFixed(3)}ms)`)
+  const undiciFetchTT = Number.parseInt(results.undici.endTime - results.undici.startTime)
+  const nodeFetchTT = Number.parseInt(results.node.endTime - results.node.startTime)
+  const minipassFetchTT = Number.parseInt(results.minipass.endTime - results.minipass.startTime)
+  console.log(`undici-fetch | total time: ${undiciFetchTT}ns (${(undiciFetchTT * 0.000001).toFixed(3)}ms)`)
+  console.log(`node-fetch | total time: ${nodeFetchTT}ns (${(nodeFetchTT * 0.000001).toFixed(3)}ms)`)
+  console.log(`minipass-fetch | total time: ${minipassFetchTT}ns (${(minipassFetchTT * 0.000001).toFixed(3)}ms)`)
   console.log('---')
   const pc1 = ((undiciFetchTT - nodeFetchTT) / nodeFetchTT) * 100
   console.log(`undici-fetch <> node-fetch percent change: ${pc1.toFixed(3)}%`)
@@ -19,109 +18,87 @@ function printResults (results, n) {
   console.log(`undici-fetch <> minipass-fetch percent change: ${pc2.toFixed(3)}%`)
 }
 
-function benchmarkParallel () { // eslint-disable-line no-unused-vars
-  const N = 1000
-
-  const server = http.createServer((req, res) => {
+if (isMainThread) {
+  const server = createServer((req, res) => {
     setTimeout(() => {
       res.end('payload')
     }, 1)
   })
 
-  const results = {
-    undiciFetch: { start: null, end: null },
-    nodeFetch: { start: null, end: null },
-    minipassFetch: { start: null, end: null }
-  }
-
-  const generateRequests = (url, client) => Array.from(Array(N)).map(() => client(url).then(res => res.text()))
-
-  const undiciFetchRequests = url => {
-    results.undiciFetch.start = microtime.now()
-    return Promise.all(generateRequests(url, undiciFetch)).then(() => {
-      results.undiciFetch.end = microtime.now()
-      undiciFetch.close()
-    })
-  }
-  const nodeFetchRequests = url => {
-    results.nodeFetch.start = microtime.now()
-    return Promise.all(generateRequests(url, nodeFetch)).then(() => {
-      results.nodeFetch.end = microtime.now()
-    })
-  }
-  const minipassFetchRequests = url => {
-    results.minipassFetch.start = microtime.now()
-    return Promise.all(generateRequests(url, minipassFetch)).then(() => {
-      results.minipassFetch.end = microtime.now()
-    })
-  }
-
   server.listen(0, () => {
+    const N = 10000
     const url = `http://localhost:${server.address().port}`
 
-    Promise.all([
-      undiciFetchRequests(url),
-      nodeFetchRequests(url),
-      minipassFetchRequests(url)
-    ]).then(() => {
-      server.close()
-      console.log(results)
-      printResults(results, N)
+    const spawnWorker = (N, url, clientType) => new Promise((resolve, reject) => {
+      const worker = new Worker(__filename, {
+        workerData: { N, url, clientType }
+      })
+      worker.on('message', resolve)
+      worker.on('error', reject)
+      worker.on('exit', code => {
+        if (code !== 0) {
+          reject(new Error(`Worker stopped with exit code ${code}`))
+        }
+      })
     })
-  })
-}
-
-function benchmarkSeries () {
-  const N = 10000
-
-  const server = http.createServer((req, res) => {
-    setTimeout(() => {
-      res.end('payload')
-    }, 1)
-  })
-
-  const results = {
-    undiciFetch: { start: null, end: null },
-    nodeFetch: { start: null, end: null },
-    minipassFetch: { start: null, end: null }
-  }
-
-  const undiciFetchRequests = async url => {
-    results.undiciFetch.start = microtime.now()
-    for (let i = 0; i < N; i++) {
-      await undiciFetch(url).then(res => res.text())
-    }
-    results.undiciFetch.end = microtime.now()
-    undiciFetch.close()
-  }
-  const nodeFetchRequests = async url => {
-    results.nodeFetch.start = microtime.now()
-    for (let i = 0; i < N; i++) {
-      await nodeFetch(url).then(res => res.text())
-    }
-    results.nodeFetch.end = microtime.now()
-  }
-  const minipassFetchRequests = async url => {
-    results.minipassFetch.start = microtime.now()
-    for (let i = 0; i < N; i++) {
-      await minipassFetch(url).then(res => res.text())
-    }
-    results.minipassFetch.end = microtime.now()
-  }
-
-  server.listen(0, () => {
-    const url = `http://localhost:${server.address().port}`
 
     Promise.all([
-      undiciFetchRequests(url),
-      nodeFetchRequests(url),
-      minipassFetchRequests(url)
-    ]).then(() => {
-      server.close()
+      spawnWorker(N, url, 'undici'),
+      spawnWorker(N, url, 'node'),
+      spawnWorker(N, url, 'minipass')
+    ]).then(values => {
+      const results = {}
+      for (const { clientType, startTime, endTime } of values) {
+        results[clientType] = { startTime, endTime }
+      }
       console.log(results)
       printResults(results, N)
+
+      server.close()
     })
   })
-}
+} else {
+  const { N, url, clientType } = workerData
 
-benchmarkSeries()
+  let fetchClient = null
+  let close = false
+  switch (clientType) {
+    case 'undici':
+      fetchClient = require('../src/fetch')()
+      close = true
+      break
+    case 'node':
+      fetchClient = require('node-fetch')
+      break
+    case 'minipass':
+      fetchClient = require('minipass-fetch')
+      break
+    default:
+      throw Error(`Invalid fetch client ${clientType}`)
+  }
+
+  const run = async (N, url, fetchClient) => {
+    const startTime = process.hrtime.bigint()
+
+    for (let i = 0; i < N; i++) {
+      await fetchClient(url)
+    }
+
+    const endTime = process.hrtime.bigint()
+
+    return { startTime, endTime }
+  }
+
+  run(N, url, fetchClient)
+    .then(({ startTime, endTime }) => {
+      if (close) {
+        fetchClient.close()
+      }
+
+      parentPort.postMessage({
+        clientType,
+        startTime,
+        endTime
+      })
+    })
+}
