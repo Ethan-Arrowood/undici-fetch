4e20ef211b24980d3aeb9d0e9fe850e24b19773a
Tim De Pauw <timdp@users.noreply.github.com>
2021-04-02 21:49:48 +0200

    Convert undici RequestAbortedError to AbortError (#30)

--- src/utils.js
+++ src/utils.js
@@ -6,6 +6,13 @@ function isReadable (obj) {
   return obj instanceof stream.Stream && typeof obj._read === 'function' && typeof obj._readableState === 'object'
 }
 
+class AbortError extends Error {
+  constructor () {
+    super('The operation was aborted')
+    this.name = 'AbortError'
+  }
+}
+
 function HeaderNameValidationError (name) {
   return TypeError(`Invalid Header name: ${name}`)
 }
@@ -81,6 +88,7 @@ function createUndiciRequestOptions (request, signal) {
 
 module.exports = {
   isReadable,
+  AbortError,
   HeaderNameValidationError,
   HeaderValueValidationError,
   validateHeaderName,
