23ef2c344e3f971f45254e69e846fa0ad33d2c1e
Ethan Arrowood <ethan.arrowood@gmail.com>
2021-05-02 12:26:14 -0600

    export undici set/get globaldispatcher methods

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict'
 
+const { setGlobalDispatcher, getGlobalDispatcher } = require('undici')
 const { fetch } = require('./src/fetch')
+
+fetch.setGlobalDispatcher = setGlobalDispatcher
+fetch.getGlobalDispatcher = getGlobalDispatcher
 
 module.exports = fetch
--- test/exports.js
+++ test/exports.js
@@ -9,3 +9,12 @@ tap.test('fetch function is default export', t => {
 
   t.ok(typeof fetch === 'function')
 })
+
+tap.test('fetch forwards undici globalDispatcher methods', t => {
+  t.plan(2)
+
+  const { setGlobalDispatcher: _setGlobalDispatcher, getGlobalDispatcher: _getGlobalDispatcher } = require('../')
+  const { setGlobalDispatcher, getGlobalDispatcher } = require('undici')
+  t.same(_setGlobalDispatcher, setGlobalDispatcher)
+  t.same(_getGlobalDispatcher, getGlobalDispatcher)
+})
