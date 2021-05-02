5f7393935afb7ed5becd9d241a99ac5479fca4ba
Raphael Eidus <raphaeleidus@users.noreply.github.com>
2021-05-02 15:56:48 -0400

    chore: update benchmarking to include libs using a shared agent (#42)
    * chore: update benchmarking to include libs using a shared agent
    
    * lint
    
    Co-authored-by: Ethan Arrowood <ethan.arrowood@gmail.com>

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -5,17 +5,18 @@ const { Worker, isMainThread, parentPort, workerData } = require('worker_threads
 
 function printResults (results, n) {
   console.log(`Results for ${n} subsequent requests: `)
-  const undiciFetchTT = Number.parseInt(results.undici.endTime - results.undici.startTime)
-  const nodeFetchTT = Number.parseInt(results.node.endTime - results.node.startTime)
-  const minipassFetchTT = Number.parseInt(results.minipass.endTime - results.minipass.startTime)
-  console.log(`undici-fetch | total time: ${undiciFetchTT}ns (${(undiciFetchTT * 0.000001).toFixed(3)}ms)`)
-  console.log(`node-fetch | total time: ${nodeFetchTT}ns (${(nodeFetchTT * 0.000001).toFixed(3)}ms)`)
-  console.log(`minipass-fetch | total time: ${minipassFetchTT}ns (${(minipassFetchTT * 0.000001).toFixed(3)}ms)`)
+  const baselineTiming = Number.parseInt(results['undici-fetch'].endTime - results['undici-fetch'].startTime)
+  Object.entries(results).forEach(([key, timing]) => {
+    const elapsedTT = Number.parseFloat(timing.endTime - timing.startTime)
+    console.log(`${key.padEnd(25)} | total time: ${elapsedTT}ns (${(elapsedTT * 0.000001).toFixed(3)}ms)`)
+  })
   console.log('---')
-  const pc1 = ((undiciFetchTT - nodeFetchTT) / nodeFetchTT) * 100
-  console.log(`undici-fetch <> node-fetch percent change: ${pc1.toFixed(3)}%`)
-  const pc2 = ((undiciFetchTT - minipassFetchTT) / minipassFetchTT) * 100
-  console.log(`undici-fetch <> minipass-fetch percent change: ${pc2.toFixed(3)}%`)
+  Object.entries(results).forEach(([key, timing]) => {
+    if (key === 'undici-fetch') return
+    const elapsedTT = Number.parseFloat(timing.endTime - timing.startTime)
+    const percent = ((baselineTiming - elapsedTT) / elapsedTT) * 100
+    console.log(`undici-fetch <> ${key} percent change: ${percent.toFixed(3)}%`)
+  })
 }
 
 if (isMainThread) {
@@ -43,9 +44,13 @@ if (isMainThread) {
     })
 
     Promise.all([
-      spawnWorker(N, url, 'undici'),
-      spawnWorker(N, url, 'node'),
-      spawnWorker(N, url, 'minipass')
+      spawnWorker(N, url, 'undici-fetch'),
+      spawnWorker(N, url, 'node-fetch'),
+      spawnWorker(N, url, 'node-fetch_with-agent'),
+      spawnWorker(N, url, 'minipass-fetch'),
+      spawnWorker(N, url, 'minipass-fetch_with-agent'),
+      spawnWorker(N, url, 'axios'),
+      spawnWorker(N, url, 'axios_with-agent')
     ]).then(values => {
       const results = {}
       for (const { clientType, startTime, endTime } of values) {
@@ -60,21 +65,64 @@ if (isMainThread) {
 } else {
   const { N, url, clientType } = workerData
 
+  const http = require('http')
+  const https = require('https')
+
   let fetchClient = null
   let close = false
   switch (clientType) {
-    case 'undici':
+    case 'undici-fetch': {
       fetchClient = require('../src/fetch')()
       close = true
       break
-    case 'node':
+    }
+    case 'node-fetch': {
       fetchClient = require('node-fetch')
       break
-    case 'minipass':
+    }
+    case 'node-fetch_with-agent': {
+      const fetch = require('node-fetch')
+      const fetchHttpAgent = new http.Agent({ keepAlive: true })
+      const fetchHttpsAgent = new https.Agent({ keepAlive: true })
+      const fetchAgent = (_parsedURL) => _parsedURL.protocol === 'http:' ? fetchHttpAgent : fetchHttpsAgent
+      fetchClient = (url) => {
+        return fetch(url, {
+          agent: fetchAgent
+        })
+      }
+      break
+    }
+    case 'minipass-fetch': {
       fetchClient = require('minipass-fetch')
       break
-    default:
+    }
+    case 'minipass-fetch_with-agent': {
+      const minipassFetch = require('minipass-fetch')
+      const mpHttpAgent = new http.Agent({ keepAlive: true })
+      const mpHttpsAgent = new https.Agent({ keepAlive: true })
+      const mpAgent = (_parsedURL) => _parsedURL.protocol === 'http:' ? mpHttpAgent : mpHttpsAgent
+      fetchClient = (url) => {
+        return minipassFetch(url, {
+          agent: mpAgent
+        })
+      }
+      break
+    }
+    case 'axios': {
+      fetchClient = require('axios')
+      break
+    }
+    case 'axios_with-agent': {
+      const axios = require('axios')
+      fetchClient = axios.create({
+        httpAgent: new http.Agent({ keepAlive: true }),
+        httpsAgent: new https.Agent({ keepAlive: true })
+      })
+      break
+    }
+    default: {
       throw Error(`Invalid fetch client ${clientType}`)
+    }
   }
 
   const run = async (N, url, fetchClient) => {
