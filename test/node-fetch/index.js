87aaf822af343bff8f25928d5b52cb50c2d41ebb
Ethan Arrowood <ethan.arrowood@gmail.com>
2021-05-09 11:37:45 -0600

    add gh_pat check

--- test/node-fetch/index.js
+++ test/node-fetch/index.js
@@ -3,9 +3,45 @@
 const { Octokit } = require('octokit')
 const { promises: fs } = require('fs')
 const { join } = require('path')
-const octokit = new Octokit({ userAgent: 'undici-fetch' })
+
+async function intializeOctokit () {
+  const warningMessage = 'Warning: gh_pat not found in .env file. You may encounter an API rate limit error for the GitHub API. To fix this, generate a PAT token (https://github.com/settings/tokens), create a .env file in the root of the undici-fetch directory, and assign the token to `gh_pat`.\nFor example: echo \'gh_pat=<paste_PAT_token_here>\' >> .env'
+
+  let ghPAT = null
+
+  try {
+    const data = await fs.readFile('.env', { encoding: 'utf-8' })
+    if (data.length === 0 || !data.includes('gh_pat')) {
+      console.warn(warningMessage)
+    } else {
+      ghPAT = data.split('=')[1]
+    }
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      console.warn(warningMessage)
+    } else {
+      throw error
+    }
+  }
+
+  return new Octokit({ userAgent: 'undici-fetch', auth: ghPAT })
+}
+
+async function fileOrDirectoryExists (path) {
+  try {
+    const stat = await fs.stat(path)
+    return stat.isDirectory() || stat.isFile()
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      return false
+    }
+    throw error
+  }
+}
 
 async function getNodeFetchTests () {
+  const octokit = await intializeOctokit()
+
   const { data: content } = await octokit.rest.repos.getContent({
     owner: 'node-fetch',
     repo: 'node-fetch'
@@ -13,8 +49,8 @@ async function getNodeFetchTests () {
 
   let { sha } = content.find(item => item.name === 'test')
   let downloadNodeFetchTests = true
-  if ((await fs.stat(join(__dirname, 'test'))).isDirectory() &&
-      (await fs.stat(join(__dirname, '.cache'))).isFile()) {
+  if (await fileOrDirectoryExists(join(__dirname, 'test')) &&
+      await fileOrDirectoryExists(join(__dirname, '.cache'))) {
     const cached = await fs.readFile(join(__dirname, '.cache'), { encoding: 'utf-8' })
     if (cached.length > 0) {
       const cachedSha = cached.split('=')[1]
@@ -43,7 +79,6 @@ async function getNodeFetchTests () {
           repo: 'node-fetch',
           file_sha: entry.sha
         })
-        console.log(data)
         await fs.writeFile(join(__dirname, 'test', entry.path), Buffer.from(content, encoding).toString('utf-8'))
       } else if (entry.type === 'tree') {
         await fs.mkdir(join(__dirname, 'test', entry.path))
