0e3ed3865ad0515d24f8d916077d4dafb096a957
Ethan Arrowood <ethan.arrowood@gmail.com>
2021-05-31 14:19:56 -0600

    export core classes and symbols

--- index.js
+++ index.js
@@ -3,6 +3,15 @@
 const { setGlobalDispatcher, getGlobalDispatcher } = require('undici')
 const { fetch } = require('./src/fetch')
 
+fetch.Request = require('./src/request').Request
+fetch.Response = require('./src/response').Response
+fetch.Headers = require('./src/headers').Headers
+fetch.Body = require('./src/body').Body
+
+fetch.internals = {
+  symbols: require('./src/symbols')
+}
+
 fetch.setGlobalDispatcher = setGlobalDispatcher
 fetch.getGlobalDispatcher = getGlobalDispatcher
 
--- test/exports.js
+++ test/exports.js
@@ -18,3 +18,22 @@ tap.test('fetch forwards undici globalDispatcher methods', t => {
   t.same(_setGlobalDispatcher, setGlobalDispatcher)
   t.same(_getGlobalDispatcher, getGlobalDispatcher)
 })
+
+tap.test('fetch exports core classes', t => {
+  t.plan(4)
+
+  const { Request, Response, Headers, Body } = require('../')
+
+  t.same(Request, require('../src/request').Request)
+  t.same(Response, require('../src/response').Response)
+  t.same(Headers, require('../src/headers').Headers)
+  t.same(Body, require('../src/body').Body)
+})
+
+tap.test('fetch exports internal symbols', t => {
+  t.plan(1)
+
+  const { internals: { symbols } } = require('../')
+
+  t.same(symbols, require('../src/symbols'))
+})
