f1f0cc342a204578a54b01deeddd03e2d8c61ab9
Ethan Arrowood <ethan@arrowood.dev>
2021-06-09 12:24:29 -0600

    Update src/utils.js

--- src/utils.js
+++ src/utils.js
@@ -4,6 +4,12 @@ function isAsyncIterable (obj) {
   return typeof obj?.[Symbol.asyncIterator] === 'function' || typeof obj?.[Symbol.iterator] === 'function'
 }
 
+/**
+ * This algorithm is based off of https://www.tbray.org/ongoing/When/200x/2003/03/22/Binary
+ * It only operates on the even indexes of the array (the header names) by only iterating at most
+ * half the length of the input array. The search also assumes all entries are strings and uses
+ * String.prototype.localeCompare for comparison
+ */
 function binarySearch (arr, val) {
   let low = 0
   let high = arr.length / 2
